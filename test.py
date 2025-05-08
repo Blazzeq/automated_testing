c8a9c6d1b147b1c18882c8284aa50b388d577ba1
Blazz <maciek.blazewicz@o2.pl>
2025-05-09 00:51:56 +0200

    Selenium demo

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("Testing2")
+from selenium import webdriver
+
+edge_options = webdriver.EdgeOptions()
+edge_options.add_argument("--window-size=1920,1080")
+edge_options.add_experimental_option("detach", True)
+browser = webdriver.Edge(edge_options)
+
+browser.get("https://demobank.jaktestowac.pl/logowanie_prod.html")
+title = browser.title
+print(title)
+assert title == 'Demobank - Strona główna - Logowanie'
+assert 'Demobank' in browser.title
+browser.quit()
